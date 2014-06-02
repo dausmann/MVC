4569b76aa9bf5b3d6944d8fd719f16470d9a6309
dausmann <christoph.meyer@live.be>
2014-06-02 22:59:28 +0200

    .

---
+++ KnockOutDemo/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using KnockOutDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KnockOutDemo.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET api/customer
+        public IEnumerable<Customer> Get()
+        {
+            var cust = CustomerRepository.GetCustomers();
+            return cust.ToList();
+        }
+
+        // GET api/customer/5
+        public Customer Get(int id)
+        {
+            return CustomerRepository.GetCustomers().Where((c) => c.Id == id).FirstOrDefault();
+        }
+
+        // POST api/customer
+        public HttpResponseMessage Post(Customer value)
+        {
+            CustomerRepository.InsertCustomer(value);
+
+            var response = Request.CreateResponse<Customer>(HttpStatusCode.Created, value);
+
+            string uri = Url.Link("DefaultApi", new { id = value.Id });
+            response.Headers.Location = new Uri(uri);
+
+            return response;
+        }
+
+        // PUT api/customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
